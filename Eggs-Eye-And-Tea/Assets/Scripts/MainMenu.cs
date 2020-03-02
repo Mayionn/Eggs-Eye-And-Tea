082f715318790b95360ce4650eb9470e1fd5058d
Mayionn <43456657+Mayionn@users.noreply.github.com>
2020-03-02 22:33:45 +0000

    Main Menu (Basics)

---
+++ Eggs-Eye-And-Tea/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    //Changes to room (1st scene)
+    public void Play()
+    {
+        SceneManager.LoadScene("Room");
+    }
+
+    //Closes game
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
