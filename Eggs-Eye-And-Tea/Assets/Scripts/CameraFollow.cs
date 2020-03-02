d50e4b0d37b45faeaefdc9ba5853ec8de2f51787
Mayionn <43456657+Mayionn@users.noreply.github.com>
2020-03-02 22:08:15 +0000

    Camera and Movement

---
+++ Eggs-Eye-And-Tea/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    //Camera Follows
+    public Transform target; //target is player
+    public float offset;     //general offset the camera is at from the player
+    public float Yoffset;    //offset the camera is at from the player on the Y axis (height so it doesnt show floor)
+
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = (Screen.height / offset);
+        //changes height depending on the height of each person's screen
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y - Yoffset, transform.position.z);
+        //because im messy and this is the only way i was able to do things, i will have to change the value of the variables Yoffset and offset
+        //once i add in the characters and the backgrounds. or else it will mess it up as i only made this to fit with the square prototype
+    }
+}
---
+++ Eggs-Eye-And-Tea/Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour {
+
+    //Movement
+    private int movementSpeed = 5;
+    public SpriteRenderer sprite;
+
+    void Update()
+    {
+        //Move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            sprite.flipX = false;
+        }
+
+        //Move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            sprite.flipX = true;                                                            //flip the sprite if it moves the left AKA press A key
+        }
+    }
+}    
+    
+
+
+
+
